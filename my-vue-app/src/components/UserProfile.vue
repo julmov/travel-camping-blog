d3325d86d958d9d0a07dc390f00d0527b7204784
julmov <ijuliamovchan@icloud.com>
2024-05-07 16:39:23 +0200

    done profile page design

--- my-vue-app/src/components/UserProfile.vue
+++ my-vue-app/src/components/UserProfile.vue
@@ -1,7 +1,21 @@
 <template>
   <div class="user-profile">
-    <h1>User Profile</h1>
-    <!-- Your profile content goes here -->
+    <div class="background-image"> 
+    </div>
+      <div class="avatar"><img src="../assets/avatar.jpg" alt="Avatar"></div>
+      <div class="profile-info-block">
+      <div>
+      <h2>John Doe</h2>
+      <p>@nickname</p>
+      </div>
+       <div class="posts-and-followers">
+      <p>posts</p>
+      <p>followers</p>
+      <p>following</p>
+      </div>
+      <button id="followBtn">Follow</button>
+    </div>
+      <p>Adipisci porro ipsum laudantium consectetur, recusandae blanditiis consequatur eveniet earum quasi autem quod corporis eligendi et repudiandae sapiente delectus quisquam ipsam ea. Excepturi facilis repellendus odit labore expedita recusandae dolorum est, eveniet dolorem laudantium tenetur quo temporibus! Modi, accusantium tempore nobis aliquid laudantium magnam quod sunt cupiditate, quasi et, facere ad possimus perspiciatis eos sed ipsum molestiae aperiam? Vero voluptatum illo quo possimus.</p>
   </div>
 </template>
 
@@ -9,6 +23,7 @@
 export default {
   name: 'UserProfile',
   // You can add component properties, data, methods, etc. here
+  
 }
 </script>
 
